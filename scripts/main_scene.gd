dccea482638f1bc740a04b8d61d25ebe6c671e4a
KorYme <92892240+KorYme@users.noreply.github.com>
2026-01-19 12:37:09 +0100

    [PROG] [Generation] : Complete size generation taken into account

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -18,20 +18,19 @@ func generate_dungeons(size : int = 5) -> void:
 	for i in range(size):
 		tab.append(sub_tab.duplicate(true))
 
+	@warning_ignore("narrowing_conversion")
 	var room_number : int = size * size * 0.5
+	@warning_ignore("integer_division")
 	var coordinates : Array[Vector2i] = [Vector2i(size / 2, 0)]
+	@warning_ignore("integer_division")
 	tab[0][size / 2] = true
 	
 	for i in range(room_number - 1):
 		coordinates.shuffle()
 		var index : int = coordinates.find_custom(
 			func(value): 
-			return (
-			(value.y + 1 < size and !tab[value.y + 1][value.x]) or
-			(value.y - 1 >= 0 and !tab[value.y - 1][value.x]) or
-			(value.x + 1 < size and !tab[value.y][value.x + 1]) or 
-			(value.x - 1 >= 0 and !tab[value.y][value.x - 1])
-			))
+			return check_coordinates(tab, value) == 0
+			)
 		var pos : Vector2i = coordinates[index]
 		
 		directions.shuffle()
@@ -43,13 +42,49 @@ func generate_dungeons(size : int = 5) -> void:
 				coordinates.append(Vector2i(dir.x + pos.x, dir.y + pos.y))
 				break
 		
+	var tmp_fill_tab : Array[Array] = tab.duplicate_deep(true)
 	for x in range(size):
 		for y in range(size):
-			if tab[y][x]:
-				var instance = room_data.Rooms.pick_random().room_reference.instantiate()
+			if tmp_fill_tab[y][x]:
+				var tmp_room : RoomData = room_data.Rooms.filter(
+					func(roomData):
+					return check_room_fit_in(roomData, tmp_fill_tab, Vector2i(x, y))
+				).pick_random()
+				tmp_fill_tab[y][x] = false
+				for dir in tmp_room.room_shape:
+					tmp_fill_tab[y + dir.y][x + dir.x] = false
+				
+				var instance = tmp_room.room_reference.instantiate()
+				@warning_ignore("integer_division")
 				instance.position.x = (x - size / 2) * room_data.RoomSize.x
 				instance.position.y = -y * room_data.RoomSize.y
 				add_child(instance)
 				if instance is Room:
 					#instance.doors
 					pass
+
+# 0 is false, 1 is true, 2 is out of bounds
+func check_coordinates(tab : Array[Array], value : Vector2i) -> int:
+	var size_y : int = tab.size()
+	if size_y <= 0:
+		return 2
+	var size_x : int = tab[0].size()
+	if size_x < 0:
+		return 2
+	if value.x >= size_x or value.y >= size_y or value.x < 0 or value.y < 0:
+		return 2
+	if value.y + 1 < size_y and tab[value.y + 1][value.x]: 
+		return 1
+	if value.y - 1 >= 0 and tab[value.y - 1][value.x]:
+		return 1
+	if value.x + 1 < size_x and tab[value.y][value.x + 1]:
+		return 1
+	if value.x - 1 >= 0 and tab[value.y][value.x - 1]:
+		return 1
+	return 0
+
+func check_room_fit_in(room : RoomData, tab : Array[Array], origin : Vector2i) -> bool:
+	for dir in room.room_shape:
+		if check_coordinates(tab, origin + dir) == 0:
+			return false
+	return true
